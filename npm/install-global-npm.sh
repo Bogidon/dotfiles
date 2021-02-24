151e439a1a5634d813ebc9a189c813557b4add6f
Bogdan Vitoc <bvitoc@nerdwallet.com>
2021-02-24 15:27:00 -0600

    refactor npm global install to allow comments

--- npm/install-global-npm.sh
+++ npm/install-global-npm.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-npm install -g \
-    git-checkout-interactive # https://github.com/CookPete/git-checkout-interactive
+modules=(
+  git-checkout-interactive # easily switch to recently used branches with `gci`
+  @jarred/git-peek # very quickly open repo with editor locally with `git peek`
+)
+
+npm install -g ${modules[@]}
