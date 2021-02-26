ba61e0612d321690bbfe06e6952e3ccfaa0c745a
Bogdan Vitoc <bvitoc@nerdwallet.com>
2021-02-25 18:07:50 -0600

    add ntl to npm globals

--- npm/install-global-npm.sh
+++ npm/install-global-npm.sh
@@ -2,6 +2,7 @@
 modules=(
   git-checkout-interactive # easily switch to recently used branches with `gci`
   @jarred/git-peek # very quickly open repo with editor locally with `git peek`
+  ntl # quickly select an npm run-script to run
 )
 
 npm install -g ${modules[@]}
