62704181fa2d5410c20abd2fee53b30a387d160f
Bogdan Vitoc <bog@bogdanvitoc.com>
2022-05-31 16:15:34 -0400

    [common] add lschmod alias

--- common/aliases/main.sh
+++ common/aliases/main.sh
@@ -5,3 +5,8 @@ alias sm='smerge'
 
 # Tmux
 alias tas="tmux attach-session"
+
+# Filesystem
+
+## `ls -l` + chmod code for each item https://stackoverflow.com/a/1796009/1888489
+alias lschmod="ls -l | awk   '{k=0;for(i=0;i<=8;i++)k+=((substr(\$1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf(\"%0o \",k);print}'"
